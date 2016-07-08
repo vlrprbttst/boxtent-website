a87b9e30a1409048c4894a482fc6137745bfbd40
Valerio <omegaiori@gmail.com>
2016-07-08 18:41:21 +0200

    fixes

--- _src/js/custom/main.js
+++ _src/js/custom/main.js
@@ -77,21 +77,21 @@ $(document).ready(function() {
         });
     });
 
-    //active link on nav
-    $(document).on("scroll", onScroll);
-    function onScroll(event) {
-        var scrollPos = $(document).scrollTop();
-        $('nav a').each(function() {
-            var currLink = $(this);
-            var refElement = $(currLink.attr("href"));
-            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                $('nav a').removeClass("active");
-                currLink.addClass("active");
-            } else {
-                currLink.removeClass("active");
-            }
-        });
-    }
+    // //active link on nav
+    // $(document).on("scroll", onScroll);
+    // function onScroll(event) {
+    //     var scrollPos = $(document).scrollTop();
+    //     $('nav a').each(function() {
+    //         var currLink = $(this);
+    //         var refElement = $(currLink.attr("href"));
+    //         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+    //             $('nav a').removeClass("active");
+    //             currLink.addClass("active");
+    //         } else {
+    //             currLink.removeClass("active");
+    //         }
+    //     });
+    // }
 
     //fit text
     $(".background-component").fitText(4, {
